4aeda697c0fbf7c20fe11d0e4bc11eac777dfe2d
Jonathan Avila <jeavila6@miners.utep.edu>
2017-12-01 13:16:31 -0700

    clean up

--- game/main.c
+++ game/main.c
@@ -1,9 +1,9 @@
 #include <msp430.h>
-#include "libTimer.h"
-#include "lcdutils.h"
-#include "lcddraw.h"
-#include "p2switches.h"
-#include "shape.h"
+#include <libTimer.h>
+#include <lcdutils.h>
+#include <lcddraw.h>
+#include <p2switches.h>
+#include <shape.h>
 #include "buzzer.h"
 //#include <abCircle.h>
 
